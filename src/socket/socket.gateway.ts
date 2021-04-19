a4433dd24c9ed48253cee097252627a6e27e6624
skkkk98 <salgu1998@gmail.com>
2021-04-20 00:37:26 +0900

    socket 기본적인거 완성

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,11 +1,20 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { 
+    WebSocketGateway, 
+    WebSocketServer, 
+    SubscribeMessage, 
+    OnGatewayConnection, 
+    OnGatewayDisconnect 
+} from '@nestjs/websockets';
 import { SocketDataDTO } from './DTO/socket-data.dto';
+import { SocketService } from './socket.service/socket.service';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer() server;
 
+    constructor( private socketservice : SocketService ) {}
+    
     async handleConnection() {
         this.server.emit('connect', "client connect");
     }
@@ -15,14 +24,24 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
 
     @SubscribeMessage('GetData')
-    async GetData(client, data){
-        if(!data.cctv_number || 
-            !data.cctv_location || !data.cctv_data)
-            console.log("no data");
-        
-        
-        
+    async GetData(client, data: SocketDataDTO){
+        if(!data.cctv_number || !data.cctv_location || !data.cctv_data) {
+                console.log("Get data error");
+                client.broadcast.emit('GetData', 'Get data error');
+
+                return;
+            }
+            
+        const saveData_Result = this.socketservice.saveData(data)
+
+        if( !saveData_Result ) {
+            console.log("save Data error");
+            client.broadcast.emit('GetData', 'save Data error');
+
+            return;
+        }
+
         client.broadcast.emit('GetData', data);
-    }s
+    }
 
 }
