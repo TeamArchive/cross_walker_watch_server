69572289f5ab8666d41af6f6adc00a3afbaf405c
skkkk98 <salgu1998@gmail.com>
2021-05-11 22:40:34 +0900

    dto test data 추가

--- src/socket/DTO/change-state-data.dto.ts
+++ src/socket/DTO/change-state-data.dto.ts
@@ -1,6 +1,15 @@
 import { IsNotEmpty, IsString } from "class-validator";
 import { cctvData } from "../entity/cctvData.entity";
 
+/**
+ * test data
+{
+	"pk":"6fcea368-6f85-4f40-ba76-5c8a03c01c1a",
+	"user":"ho lee fuck",
+	"cctv_state":"success",
+	"success_at":"2021-05-11 22:15:11.482553"
+}
+ */
 export class changeStateDataDTO {
     @IsNotEmpty()
 	@IsString()
